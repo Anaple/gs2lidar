9a01b9618a291f89fcdb98f744b579493a18d756
Anaple <67675386+Anaple@users.noreply.github.com>
2024-03-05 13:59:06 +0800

    rm

--- build/gs2lidar/CMakeFiles/gs2lidar_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gs2lidar_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gs2lidar_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/gs2lidar/CMakeFiles/gs2lidar_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for gs2lidar_uninstall.
